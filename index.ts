bb80d067748c69f27ed4af1f103bfde8659b1d3f
updaun <updauney@daum.net>
2023-03-30 23:49:25 +0900

    #2.0 How Typescript Works

---
+++ index.ts
@@ -0,0 +1,21 @@
+const nico = {
+    nickname: "nick"
+}
+
+nico.hello()
+
+result = [1, 2, 3, 4] + false
+console.log(result)
+
+function divide(a, b) {
+    return a / b
+}
+
+divide("hello")
+
+const player = {
+    age: 12
+}
+
+player.age = false
+
