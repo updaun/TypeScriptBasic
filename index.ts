955b389f52d3abf908c85764d9b4df349b39fb45
updaun <updauney@daum.net>
2023-04-02 03:51:47 +0900

    #2.4 Types of TS part Three

--- index.ts
+++ index.ts
@@ -39,3 +39,31 @@ const player: readonly [string, number, boolean] = ["daun", 12, true];
 const a: any[] = [1, 2, 3, 4]
 const b: any = true
 a + b
+
+let c: unknown;
+if (typeof c === "number") {
+    let d = c + 1;
+}
+if (typeof c === "string") {
+    c.toUpperCase();
+}
+// none return function
+function hello() {
+    console.log("x");
+}
+
+// function never return
+function nice(): never {
+    throw new Error("x");
+}
+
+function hi(name: string | number) {
+    if (typeof name === "string") {
+        name
+    } else if (typeof name === "number") {
+        name
+    } else {
+        name
+    }
+
+}
