8cd68d5eb42fee596c50c3a7bedc402ccf911453
updaun <updauney@daum.net>
2023-04-16 19:31:51 +0900

    #5.7 Chain

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,38 @@ class Block implements BlockShape{
         public height: number,
         public data: string,
     ) {
-        this.hash = calculateHash(prevHash, height, data);
+        this.hash = Block.calculateHash(prevHash, height, data);
     }
     static calculateHash(prevHash: string, height: number, data: string) {
         const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain {
+    private blocks: Block[]
+    constructor() {
+        this.blocks = [];
+    }
+    private getPrevHash() {
+        if (this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    public addBlock(data: string) {
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+    public getBlocks() {
+        return [...this.blocks]; 
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+
+blockchain.getBlocks().push(new Block("xxxxxx", 11111, "Hacked!"))
+
+console.log(blockchain.getBlocks());
